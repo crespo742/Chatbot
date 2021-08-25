16b68156afdfa39cdc92e36657b8b3d28009c39a
crespo <matthiascrespo9@gmail.com>
2021-08-25 20:41:33 +0200

    envoi du code dans index.js

--- app.js
+++ app.js
@@ -1,53 +1,53 @@
-const request = require('request');
+// const request = require('request');
 
-const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
-const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
+// const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+// const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
 
-// Handles messages events
-function handleMessage(sender_psid, received_message) {
+// // Handles messages events
+// function handleMessage(sender_psid, received_message) {
 
-  let response;
+//   let response;
 
-  // Check if the message contains text
-  if (received_message.text) {    
+//   // Check if the message contains text
+//   if (received_message.text) {    
 
-    // Create the payload for a basic text message
-    response = {
-      "text": `You sent the message: "${received_message.text}". Now send me an image!`
-    }
-  }  
+//     // Create the payload for a basic text message
+//     response = {
+//       "text": `You sent the message: "${received_message.text}". Now send me an image!`
+//     }
+//   }  
   
-  // Sends the response message
-  callSendAPI(sender_psid, response);    
-}
+//   // Sends the response message
+//   callSendAPI(sender_psid, response);    
+// }
 
 
-// Handles messaging_postbacks events
-function handlePostback(sender_psid, received_postback) {
+// // Handles messaging_postbacks events
+// function handlePostback(sender_psid, received_postback) {
 
-}
+// }
 
-// Sends response messages via the Send API
-function callSendAPI(sender_psid, response) {
-  // Construct the message body
-  let request_body = {
-    "recipient": {
-      "id": sender_psid
-    },
-    "message": response
-  }
+// // Sends response messages via the Send API
+// function callSendAPI(sender_psid, response) {
+//   // Construct the message body
+//   let request_body = {
+//     "recipient": {
+//       "id": sender_psid
+//     },
+//     "message": response
+//   }
 
-  // Send the HTTP request to the Messenger Platform
-  request({
-    "uri": "https://graph.facebook.com/v2.6/me/messages",
-    "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
-    "method": "POST",
-    "json": request_body
-  }, (err, res, body) => {
-    if (!err) {
-      console.log('message sent!')
-    } else {
-      console.error("Unable to send message:" + err);
-    }
-  }); 
-}
+//   // Send the HTTP request to the Messenger Platform
+//   request({
+//     "uri": "https://graph.facebook.com/v2.6/me/messages",
+//     "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
+//     "method": "POST",
+//     "json": request_body
+//   }, (err, res, body) => {
+//     if (!err) {
+//       console.log('message sent!')
+//     } else {
+//       console.error("Unable to send message:" + err);
+//     }
+//   }); 
+// }
