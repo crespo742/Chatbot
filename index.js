186042c1cfba7c80c52b18757bce644f0de38ae6
crespo <matthiascrespo9@gmail.com>
2021-08-26 17:35:36 +0200

    changements de message dans le log

--- index.js
+++ index.js
@@ -168,7 +168,7 @@ function callSendAPI(sender_psid, response) {
     "json": request_body
   }, (err, res, body) => {
     if (!err) {
-      console.log('message sent!')
+      console.log('message envoyer !')
     } else {
       console.error("Unable to send message:" + err);
     }
